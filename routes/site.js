acc4f456703340c600ef4321b662fe403fc17654
BuildTools <francochen2000@hotmail.com>
2017-05-22 11:59:58 -0400

    Cleaned up partials

--- routes/site.js
+++ routes/site.js
@@ -3,13 +3,6 @@ module.exports.index = function (req, res) {
   const session = req.session;
 
   res.render("index", {
-    "brand": {
-      "name": "Shine"
-    },
-    "libraries": {
-      "jquery": "3.2.1",
-      "bootstrap": "3.3.7"
-    },
     "user": session.user
   });
 };
